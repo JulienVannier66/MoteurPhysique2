daffa558a5549aed2139219318f909d99d69b553
Simon lauzeral <sim78100@hotmail.fr>
2019-11-14 11:42:19 -0500

    creation de la classe RigidBody

---
+++ main/myexe/include/RigidBody.hpp
@@ -0,0 +1,22 @@
+#include <Vecteur3D.hpp>
+
+class RigidBody
+{
+private:
+    float m_inverseMass;
+    float m_masse;
+    float m_linearDumping;
+    Vecteur3D m_position;
+    Vecteur3D m_velocite;
+    //Quaternion m_orientation;
+    Vecteur3D m_rotation;	// velocite angulaire
+	//Matrix4 m_transforme Matrix
+
+public:
+    /*Constructeurs et destructeur*/
+	RigidBody(float p_masse, float p_linearDumping, float p_position, float p_velocite, float p_rotation, Quaternion p_orientation) 
+	{
+
+	}
+
+};
