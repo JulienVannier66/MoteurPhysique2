2a46efea3f0055dc8f941e5c35e9fade1c71ca4a
Julien <julien.vannier66@gmail.com>
2019-11-19 23:43:06 -0500

    Ajout de freegut + test dans main

--- main/myexe/src/main.cpp
+++ main/myexe/src/main.cpp
@@ -1,38 +1,113 @@
+#include "../../../external/freeglut-3.2.0/include/GL/freeglut_std.h"
 #include <iostream>
-#include <Vecteur3D.hpp>
 
-void testVecteur3D();
+void inputKeyBoard(unsigned char key, int x, int y);
+void renderScene(void);
 
-int main(void)
+float MatSpec[4] = {0.1f, 0.1f, 0.5f, 1.0f};
+float MatDif[4] = {0.057f, 0.447f, 0.361f, 1.0f};
+float MatAmb[4] = {0.3f, 0.3f, 0.3f, 1.0f};
+float Light1Pos[4] = {0.0f, 0.0f, 20.0f, 1.0f};
+float Light1Dif[4] = {0.0f, 0.0f, 0.0f, 1.0f};
+float Light1Spec[4] = {0.0f, 1.0f, 1.0f, 1.0f};
+float Light1Amb[4] = {0.5f, 0.5f, 0.5f, 1.0f};
+float Spot1Dir[3] = {0.0f, 0.0f, -1.0f};
+GLfloat position1[4] = {-5.0, 0.0, 3.0, 0.0};
+GLfloat shine[1] = {50.0};
+GLfloat Lnoire[4] = {0.0, 0.0, 0.0, 1.0};
+
+float i = 0;
+int nbFrames = 0;
+float lastTime = 0;
+
+void inputKeyBoard(unsigned char key, int x, int y) { std::cout << "t" << std::endl; }
+// Calculate FPS
+void calculFrame(int& nbFrames, float& lastTime)
 {
-    testVecteur3D();
-    std::cin.ignore();
+    double currentTime = glutGet(GLUT_ELAPSED_TIME);
+    nbFrames++;
+    if (currentTime - lastTime >= 1000)
+    {
+        // cout and reset timer
+        std::cout << "FPS : " << nbFrames << std::endl;
+        nbFrames = 0;
+        lastTime += 1000;
+    }
 }
 
-void testVecteur3D()
+void changeSize(int w, int h)
 {
-    std::cout << "Hello World!\n";
-    Vecteur3D v(2, 6, 7);
-    Vecteur3D v2(4, -6, 3);
-    Vecteur3D v3;
 
-    std::cout << v << "\n";
-    std::cout << v2 << "\n";
-    std::cout << v3 << "\n";
+    // Prevent a divide by zero, when window is too short
+    if (h == 0) { h = 1; }
 
-    v3 = v + v2;
-    std::cout << v3 << "\n";
+    float ratio = w * 1.0 / h;
 
-    v3 = v - v2;
-    std::cout << v3 << "\n";
+    // Use the Projection Matrix
+    glMatrixMode(GL_PROJECTION);
 
-    v3 = v * 2;
-    std::cout << v3 << "\n";
+    // Reset Matrix
+    glLoadIdentity();
 
-    v3 = v ^ v2;
-    std::cout << v3 << "\n";
+    // Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
 
-    std::cout << v.produitScalaire(v2) << "\n";
-    std::cout << v.norme() << "\n";
-    std::cout << v2.norme() << "\n";
+    // Set the correct perspective.
+    gluPerspective(45, ratio, 1, 100);
+
+    // Get Back to the Modelview
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void renderScene(void)
+{
+    calculFrame(nbFrames, lastTime);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+
+	glTranslatef(-60, -10, -50);
+    glRotated(0.2 * i, 0, 0, 1);
+
+    glBegin(GL_QUADS);
+    glVertex3f(-2, -2, 0.0);
+    glVertex3f(-2, 2, 0.0);
+    glVertex3f(2, 2, 0.0);
+    glVertex3f(2, -2, 0.0);
+    glEnd();
+        /*glBegin(GL_TRIANGLES);
+        glVertex3f(0.0, 2, 0.0);
+        glVertex3f(2, 0.0, 0.0);
+        glVertex3f(2, 2, 0.0);
+        glEnd();*/
+
+    glPopMatrix();
+    glFlush();
+
+	glutPostRedisplay();
+    glutSwapBuffers();
+
+	i = glutGet(GLUT_ELAPSED_TIME);
+}
+
+int main(int argc, char** argv)
+{
+
+    // init GLUT and create window
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(1000, 320);
+    glutCreateWindow("Moteur physique Particule");
+
+    // register callbacks
+    glutDisplayFunc(renderScene);
+    glutReshapeFunc(changeSize);
+    glutKeyboardFunc(inputKeyBoard);
+
+    // enter GLUT event processing loop
+    glutMainLoop();
+
+    return 1;
 }
