cc2d2daeb647326ce9f58e21c526abe88e869d3e
Pierrick <barbarroux.pierrick@outlook.fr>
2019-12-10 22:33:06 -0500

    update octree (pas terminé)

--- main/myexe/include/Octree.hpp
+++ main/myexe/include/Octree.hpp
@@ -1,23 +1,23 @@
 #pragma once
-#include <BoundingBox.hpp>
-#include <BoundingSphere.hpp>
-#include <iostream>       
-#include <deque>         
-#include <list>          
-#include <queue>  
 #include <RigidBody.hpp>
-
+#include <Rectangle.hpp>
+#include <Vecteur3D.hpp>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <vector>
+#include <queue>
 
 typedef unsigned char BYTE;
 
 class Octree
 {
 private:
-    BoundingBox m_region;
-    //std::vector<BoundingSphere> m_spheres_list; // liste des objets présent dans la bounding box
-    std::list<RigidBody> m_objects; 
+    Rectangle m_region;
+    // std::vector<BoundingSphere> m_spheres_list; // liste des objets présent dans la bounding box
+    std::vector<RigidBody::BoundingSphere> m_objects;
 
-    static std::queue<RigidBody> m_pendingInsertion;
+    static std::queue<RigidBody::BoundingSphere> m_pendingInsertion;
 
     // OctTree[] m_childNode = new OctTree[8];
     // les subdivisions de notre octree
@@ -38,24 +38,34 @@ private:
     int m_curLife = -1;    // this is a countdown time showing how much time we have left to live
 
     /// A reference to the parent node is nice to have when we're trying to do a tree update.
-    Octree m_parent;
+    Octree& m_parent;
+
     static bool m_treeReady = false; // the tree has a few objects which need to be inserted before it is complete
     static bool m_treeBuilt = false; // there is no pre-existing tree yet.
 
 public:
     // Constructors
-    Octree()
-    {
-        m_objects = new std::list<RigidBody>();
-        m_region = new BoundingBox(Vector3.Zero, Vector3.Zero);
+    Octree(){
+        Vecteur3D vecteur1 = Vecteur3D(0, 0, 0);
+        Vecteur3D vecteur2 = Vecteur3D(0, 0, 0);
+        //m_parent = ;
+		 
+        m_objects = std::vector<RigidBody::BoundingSphere>();
+        m_region = Rectangle(vecteur1, vecteur2);
         m_curLife = -1;
-    };
-    Octree(BoundingBox p_region, std::list<RigidBody> p_objList);
-    Octree(BoundingBox p_region);
+    }
+    Octree(Rectangle p_region, std::vector<RigidBody::BoundingSphere> p_objList);
+    Octree(Rectangle p_region);
 
     /// Processes all pending insertions by inserting them into the tree.
     void UpdateTree();
 
-	/// Naively builds an oct tree from scratch.
+    /// Naively builds an oct tree from scratch.
     void BuildTree();
+
+    Octree CreateNode(Rectangle p_region, std::vector<RigidBody::BoundingSphere> p_objList); // complete & tested
+
+    Octree CreateNode(Rectangle p_region, RigidBody::BoundingSphere p_item);
+
+	//bool Octree::Insert<T>(T Item) where T : Physical;
 };
--- main/myexe/src/Octree.cpp
+++ main/myexe/src/Octree.cpp
@@ -8,7 +8,7 @@
 
 /// The bounding region for the oct tree.
 /// The list of objects contained within the bounding region
-Octree::Octree(BoundingBox p_region, std::list<RigidBody> p_objList)
+Octree::Octree(Rectangle p_region, std::vector<RigidBody::BoundingSphere> p_objList)
 {
     m_region = p_region;
     m_objects = p_objList;
@@ -21,10 +21,10 @@ Octree::Octree(BoundingBox p_region, std::list<RigidBody> p_objList)
 
 /// The suggested dimensions for the bounding region.
 /// Note: if items are outside this region, the region will be automatically resized.
-Octree::Octree(BoundingBox p_region)
+Octree::Octree(Rectangle p_region)
 {
     m_region = p_region;
-    m_objects = std::list<RigidBody>();
+    m_objects = std::vector<RigidBody::BoundingSphere>();
     m_curLife = -1;
 }
 
@@ -32,12 +32,16 @@ void Octree::UpdateTree()
 {
     if (!m_treeBuilt)
     {
-        while (m_pendingInsertion.Count != 0) m_objects.Add(m_pendingInsertion.Dequeue());
+        while (m_pendingInsertion.size() != 0)
+        {
+            m_objects.push_back(m_pendingInsertion.front());
+            m_pendingInsertion.pop();
+        }
         BuildTree();
     }
     else
     {
-        while (m_pendingInsertion.Count != 0) Insert(m_pendingInsertion.Dequeue());
+        while (m_pendingInsertion.size() != 0) { Insert(m_pendingInsertion.pop()); }
     }
     m_treeReady = true;
 }
@@ -45,80 +49,83 @@ void Octree::UpdateTree()
 void Octree::BuildTree() // complete & tested
 {
     // terminate the recursion if we're a leaf node
-    if (m_objects.Count <= 1) return;
+    if (m_objects.size() <= 1) return;
 
-    Vector3 dimensions = m_region.Max - m_region.Min;
+    Vecteur3D dimensions = m_region.getMax() - m_region.getMin();
 
-    if (dimensions == Vector3.Zero)
+    Vecteur3D vecteur0 = Vecteur3D(0, 0, 0);
+
+    if (dimensions == vecteur0)
     {
         FindEnclosingCube();
-        dimensions = m_region.Max - m_region.Min;
+        dimensions = m_region.getMax() - m_region.getMin();
     }
 
     // Check to see if the dimensions of the box are greater than the minimum dimensions
-    if (dimensions.X <= MIN_SIZE && dimensions.Y <= MIN_SIZE && dimensions.Z <= MIN_SIZE)
+    if (dimensions.getX() <= m_taille_min && dimensions.getY() <= m_taille_min &&
+        dimensions.getZ() <= m_taille_min)
     { return; }
 
-    Vector3 half = dimensions / 2.0f;
-    Vector3 center = m_region.Min + half;
+    Vecteur3D half = dimensions * 0.5f;
+    Vecteur3D center = m_region.getMin() + half;
 
     // Create subdivided regions for each octant
-    BoundingBox[] octant = new BoundingBox[8];
-    octant[0] = new BoundingBox(m_region.Min, center);
-    octant[1] = new BoundingBox(new Vector3(center.X, m_region.Min.Y, m_region.Min.Z),
-                                new Vector3(m_region.Max.X, center.Y, center.Z));
-    octant[2] = new BoundingBox(new Vector3(center.X, m_region.Min.Y, center.Z),
-                                new Vector3(m_region.Max.X, center.Y, m_region.Max.Z));
-    octant[3] = new BoundingBox(new Vector3(m_region.Min.X, m_region.Min.Y, center.Z),
-                                new Vector3(center.X, center.Y, m_region.Max.Z));
-    octant[4] = new BoundingBox(new Vector3(m_region.Min.X, center.Y, m_region.Min.Z),
-                                new Vector3(center.X, m_region.Max.Y, center.Z));
-    octant[5] = new BoundingBox(new Vector3(center.X, center.Y, m_region.Min.Z),
-                                new Vector3(m_region.Max.X, m_region.Max.Y, center.Z));
-    octant[6] = new BoundingBox(center, m_region.Max);
-    octant[7] = new BoundingBox(new Vector3(m_region.Min.X, center.Y, center.Z),
-                                new Vector3(center.X, m_region.Max.Y, m_region.Max.Z));
+    std::vector<Rectangle> octant;
+    // Rectangle[] octant = new Rectangle[8];
+    octant.push_back(Rectangle(m_region.getMin(), center));
+    octant.push_back(
+        Rectangle(Vecteur3D(center.getX(), m_region.getMin().getY(), m_region.getMin().getZ()),
+                  Vecteur3D(m_region.getMax().getX(), center.getY(), center.getZ())));
+    octant.push_back(
+        Rectangle(Vecteur3D(center.getX(), m_region.getMin().getY(), center.getZ()),
+                  Vecteur3D(m_region.getMax().getX(), center.getY(), m_region.getMax().getZ())));
+    octant.push_back(
+        Rectangle(Vecteur3D(m_region.getMin().getX(), m_region.getMin().getY(), center.getZ()),
+                  Vecteur3D(center.getX(), center.getY(), m_region.getMax().getZ())));
+    octant.push_back(
+        Rectangle(Vecteur3D(m_region.getMin().getX(), center.getY(), m_region.getMin().getZ()),
+                  Vecteur3D(center.getX(), m_region.getMax().getY(), center.getZ())));
+    octant.push_back(
+        Rectangle(Vecteur3D(center.getX(), center.getY(), m_region.getMin().getZ()),
+                  Vecteur3D(m_region.getMax().getX(), m_region.getMax().getY(), center.getZ())));
+    octant.push_back(Rectangle(center, m_region.getMax()));
+    octant.push_back(
+        Rectangle(Vecteur3D(m_region.getMin().getX(), center.getY(), center.getZ()),
+                  Vecteur3D(center.getX(), m_region.getMax().getY(), m_region.getMax().getZ())));
 
     // This will contain all of our objects which fit within each respective octant.
-    List[] octList = new List[8];
+    std::vector<std::vector<RigidBody::BoundingSphere>> octList;
+    //= std::vector<RigidBody::BoundingSphere>;
 
-    for (int i = 0; i < 8; i++) octList = new List();
+    // for (int i = 0; i < 8; i++) { octList = new List(); }
 
     // this list contains all of the objects which got moved down the tree and can be delisted from
     // this node.
-    List delist = new List();
+    std::vector<RigidBody::BoundingSphere> delist;
 
-    foreach (Physical obj in m_objects)
+    for (RigidBody::BoundingSphere obj : m_objects)
     {
-        if (obj.BoundingBox.Min != obj.BoundingBox.Max)
+        if (obj.rayon != 0)
         {
-            for (int a = 0; a < 8; a++)
+            for (int i = 0; i < 8; i++)
             {
-                if (octant[a].Contains(obj.BoundingBox) == ContainmentType.Contains)
+				if (!octant.at(i).empty()) {
+
+				}
+               /* if (octant[a].Contains(obj.BoundingSphere) == ContainmentType.Contains)
                 {
                     octList[a].Add(obj);
                     delist.Add(obj);
                     break;
-                }
-            }
-        }
-        else if (obj.BoundingSphere.Radius != 0)
-        {
-            for (int a = 0; a < 8; a++)
-            {
-                if (octant[a].Contains(obj.BoundingSphere) == ContainmentType.Contains)
-                {
-                    octList[a].Add(obj);
-                    delist.Add(obj);
-                    break;
-                }
+                }*/
             }
         }
     }
 
     // delist every moved object from this node.
-    foreach (Physical obj in delist)
-        m_objects.Remove(obj);
+    for (RigidBody::BoundingSphere obj : delist) { 
+		m_objects.remove(obj);
+	}
 
     // Create child nodes where there are items contained in the bounding region
     for (int a = 0; a < 8; a++)
@@ -134,3 +141,185 @@ void Octree::BuildTree() // complete & tested
     m_treeBuilt = true;
     m_treeReady = true;
 }
+
+Octree Octree::CreateNode(Rectangle region, std::vector<RigidBody::BoundingSphere> objList) // complete & tested
+{
+    if (objList.size() == 0) return null;
+    Octree ret = Octree(region, objList);
+    ret.m_parent = *this;
+    return ret;
+}
+
+Octree Octree::CreateNode(Rectangle p_region, RigidBody::BoundingSphere p_item)
+{
+    std::vector<RigidBody::BoundingSphere> objList; // sacrifice potential CPU time for a smaller memory
+													// footprint
+    objList.push_back(p_item);
+    Octree ret = Octree(p_region, objList);
+    ret.m_parent = this;
+    return ret;
+}
+
+/// <summary>
+/// A tree has already been created, so we're going to try to insert an item into the tree without
+/// rebuilding the whole thing
+/// </summary>
+/// <typeparam name="T">A physical object</typeparam>
+/// <param name="Item">The physical object to insert into the tree</param>
+//bool Octree::Insert<T>(T Item) where T : Physical
+//{
+//    /*if the current node is an empty leaf node, just insert and leave it.*/
+//    // if (m_objects.Count == 0 && m_activeNodes == 0)
+//    if (AllTreeObjects.Count == 0)
+//    {
+//        m_objects.Add(Item);
+//        return true;
+//    }
+//
+//    // Check to see if the dimensions of the box are greater than the minimum dimensions.
+//    // If we're at the smallest size, just insert the item here. We can't go any lower!
+//    Vecteur3D dimensions = m_region.getMax() - m_region.getMin();
+//    if (dimensions.getX() <= m_taille_min && dimensions.getY() <= m_taille_min &&
+//        dimensions.getZ() <= m_taille_min)
+//    {
+//        m_objects.Add(Item);
+//        return true;
+//    }
+//
+//    // The object won't fit into the current region, so it won't fit into any child regions.
+//    // therefore, try to push it up the tree. If we're at the root node, we need to resize the whole
+//    // tree.
+//    if (m_region.Contains(Item.EnclosingSphere) != ContainmentType.Contains)
+//    {
+//        if (this._parent != null)
+//            return this._parent.Insert(Item);
+//        else
+//            return false;
+//    }
+//
+//    // At this point, we at least know this region can contain the object but there are child nodes.
+//    // Let's try to see if the object will fit within a subregion of this region.
+//
+//    Vecteur3D half = dimensions / 2.0f;
+//    Vecteur3D center = m_region.getMin() + half;
+//
+//    // Find or create subdivided regions for each octant in the current region
+//    BoundingBox[] childOctant = new BoundingBox[8];
+//    childOctant[0] = (m_childNode[0] != null) ? m_childNode[0].m_region
+//                                              : new BoundingBox(m_region.getMin(), center);
+//    childOctant[1] =
+//        (m_childNode[1] != null)
+//            ? m_childNode[1].m_region
+//            : new BoundingBox(
+//                  new Vecteur3D(center.getX(), m_region.getMin().getY(), m_region.getMin().getZ()),
+//                  new Vecteur3D(m_region.getMax().getX(), center.getY(), center.getZ()));
+//    childOctant[2] =
+//        (m_childNode[2] != null)
+//            ? m_childNode[2].m_region
+//            : new BoundingBox(
+//                  new Vecteur3D(center.getX(), m_region.getMin().getY(), center.getZ()),
+//                  new Vecteur3D(m_region.getMax().getX(), center.getY(), m_region.getMax().getZ()));
+//    childOctant[3] =
+//        (m_childNode[3] != null)
+//            ? m_childNode[3].m_region
+//            : new BoundingBox(
+//                  new Vecteur3D(m_region.getMin().getX(), m_region.getMin().getY(), center.getZ()),
+//                  new Vecteur3D(center.getX(), center.getY(), m_region.getMax().getZ()));
+//    childOctant[4] =
+//        (m_childNode[4] != null)
+//            ? m_childNode[4].m_region
+//            : new BoundingBox(
+//                  new Vecteur3D(m_region.getMin().getX(), center.getY(), m_region.getMin().getZ()),
+//                  new Vecteur3D(center.getX(), m_region.getMax().getY(), center.getZ()));
+//    childOctant[5] =
+//        (m_childNode[5] != null)
+//            ? m_childNode[5].m_region
+//            : new BoundingBox(
+//                  new Vecteur3D(center.getX(), center.getY(), m_region.getMin().getZ()),
+//                  new Vecteur3D(m_region.getMax().getX(), m_region.getMax().getY(), center.getZ()));
+//    childOctant[6] = (m_childNode[6] != null) ? m_childNode[6].m_region
+//                                              : new BoundingBox(center, m_region.getMax());
+//    childOctant[7] =
+//        (m_childNode[7] != null)
+//            ? m_childNode[7].m_region
+//            : new BoundingBox(
+//                  new Vecteur3D(m_region.getMin().getX(), center.getY(), center.getZ()),
+//                  new Vecteur3D(center.getX(), m_region.getMax().getY(), m_region.getMax().getZ()));
+//
+//    // First, is the item completely contained within the root bounding box?
+//    // note2: I shouldn't actually have to compensate for this. If an object is out of our
+//    // predefined bounds, then we have a problem/error.
+//    //          Wrong. Our initial bounding box for the terrain is constricting its height to the
+//    //          highest peak. Flying units will be above that.
+//    //             Fix: I resized the enclosing box to 256x256x256. This should be sufficient.
+//    if (Item.EnclosingBox.Max != Item.EnclosingBox.Min &&
+//        m_region.Contains(Item.EnclosingBox) == ContainmentType.Contains)
+//    {
+//        bool found = false;
+//        // we will try to place the object into a child node. If we can't fit it in a child node,
+//        // then we insert it into the current node object list.
+//        for (int a = 0; a < 8; a++)
+//        {
+//            // is the object fully contained within a quadrant?
+//            if (childOctant[a].Contains(Item.EnclosingBox) == ContainmentType.Contains)
+//            {
+//                if (m_childNode[a] != null)
+//                {
+//                    return m_childNode[a].Insert(Item); // Add the item into that tree and let the
+//                                                        // child tree figure out what to do with it
+//                }
+//                else
+//                {
+//                    m_childNode[a] =
+//                        CreateNode(childOctant[a], Item); // create a new tree node with the item
+//                    m_activeNodes |= (byte)(1 << a);
+//                }
+//                found = true;
+//            }
+//        }
+//
+//        // we couldn't fit the item into a smaller box, so we'll have to insert it in this region
+//        if (!found)
+//        {
+//            m_objects.Add(Item);
+//            return true;
+//        }
+//    }
+//    else if (Item.EnclosingSphere.Radius != 0 &&
+//             m_region.Contains(Item.EnclosingSphere) == ContainmentType.Contains)
+//    {
+//        bool found = false;
+//        // we will try to place the object into a child node. If we can't fit it in a child node,
+//        // then we insert it into the current node object list.
+//        for (int a = 0; a < 8; a++)
+//        {
+//            // is the object contained within a child quadrant?
+//            if (childOctant[a].Contains(Item.EnclosingSphere) == ContainmentType.Contains)
+//            {
+//                if (m_childNode[a] != null)
+//                {
+//                    return m_childNode[a].Insert(Item); // Add the item into that tree and let the
+//                                                        // child tree figure out what to do with it
+//                }
+//                else
+//                {
+//                    m_childNode[a] =
+//                        CreateNode(childOctant[a], Item); // create a new tree node with the item
+//                    m_activeNodes |= (byte)(1 << a);
+//                }
+//                found = true;
+//            }
+//        }
+//
+//        // we couldn't fit the item into a smaller box, so we'll have to insert it in this region
+//        if (!found)
+//        {
+//            m_objects.Add(Item);
+//            return true;
+//        }
+//    }
+//
+//    // either the item lies outside of the enclosed bounding box or it is intersecting it. Either
+//    // way, we need to rebuild the entire tree by enlarging the containing bounding box
+//    return false;
+//}
